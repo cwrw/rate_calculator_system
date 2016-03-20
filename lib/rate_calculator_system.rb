7727700e7a825b71ed50d287d71cf6bc57d2d453
Abeer Salameh <abeer.salameh@gmail.com>
2016-03-20 17:52:59 +0000

    Tie everything together in the calculator system module with calculate method
    Fix the feature tests approriatly

--- lib/rate_calculator_system.rb
+++ lib/rate_calculator_system.rb
@@ -1,6 +1,15 @@
 require "csv"
-require "rate_calculator_system/lender"
-require "rate_calculator_system/exceptions"
-require "rate_calculator_system/input_parser"
-require "rate_calculator_system/calculator"
 require "pry-byebug"
+require "./lib/rate_calculator_system/lender"
+require "./lib/rate_calculator_system/exceptions"
+require "./lib/rate_calculator_system/input_parser"
+require "./lib/rate_calculator_system/calculator"
+
+module RateCalculatorSystem
+  def self.calculate(file, amount)
+    lenders = RateCalculatorSystem::InputParser.new(file).parse
+    puts RateCalculatorSystem::Calculator.new(amount, lenders).final_rate
+  rescue => e
+    puts "Could not calculate rate due to the following: #{e.message}"
+  end
+end
--- spec/features/rate_calculator_system_spec.rb
+++ spec/features/rate_calculator_system_spec.rb
@@ -1,41 +1,80 @@
 require "spec_helper"
+
 RSpec.feature 'Rate calculator system', type: feature do
   let(:path) { File.dirname(__FILE__) }
   let(:market_data) do
-    File.open(File.join(path, "../support/fixtures/market_data.csv"))
-  end
-  let(:invalid_data) do
-    File.open(File.join(path, "../support/fixtures/invalid_data.csv"))
+    File.join(path, "../support/fixtures/market_data.csv")
   end
 
-  xcontext "valid data" do
+  let(:invalid_data) do
+    File.join(path, "../support/fixtures/invalid_data.csv")
+  end
+
+  let(:invalid_header) do
+    File.join(path, "../support/fixtures/invalid_header.csv")
+  end
+
+  subject { RateCalculatorSystem.calculate(market_data, loan) }
+
+  context "valid data" do
+    let(:message) do
+      %r{
+        Requested amount: £1100
+        Rate: 7.0%
+        Monthly repayment: £112.37
+        Total repayment: £1348.45
+      }
+    end
     context "loan within range" do
+      let(:loan) { "1100" }
       scenario "outputs lowest rate details" do
-        RateCalculatorSystem.calculate(market_data, "1100")
+        expect { subject }.to output(message).to_stdout
       end
     end
 
     context "loan out of range" do
-      scenario "outputs relevant out of range message when less" do
-        RateCalculatorSystem.calculate(market_data, "900")
+      let(:loan) { "15_100" }
+      let(:message) do
+        %r{Invalid loan value}
       end
 
-      scenario "outputs relevant out of range message when more" do
-        RateCalculatorSystem.calculate(market_data, "15100")
+      scenario "outputs relevant out of range message" do
+        expect { subject }.to output(message).to_stdout
+      end
+    end
+
+    context "not enought funds available" do
+      let(:loan) { "3000" }
+      let(:message) do
+        %r{There aren't enough funds to satisfy the loan value}
+      end
+
+      scenario "outputs relevant no funds message" do
+        expect { subject }.to output(message).to_stdout
       end
     end
   end
 
-  xcontext "invalid data" do
+  context "invalid data" do
     context "market data" do
-      scenario "outputs relevant message" do
-        RateCalculatorSystem.calculate(invalid_data, "1100")
+      let(:loan) { "1100" }
+
+      scenario "outputs relevant invalid file message" do
+        expect { RateCalculatorSystem.calculate(invalid_data, loan) }.to output(%r{invalid value for Float}).to_stdout
+      end
+
+      scenario "outputs relevant invalid header message" do
+        expect { RateCalculatorSystem.calculate(invalid_header, loan) }.to output(%r{Invalid Header in CSV file}).to_stdout
       end
     end
 
     context "requested loan" do
+      let(:loan) { "Boom" }
+      let(:message) do
+        %r{invalid value for Float}
+      end
       scenario "outputs relevant message" do
-        RateCalculatorSystem.calculate(market_data, "Boom")
+        expect { subject }.to output(message).to_stdout
       end
     end
   end
