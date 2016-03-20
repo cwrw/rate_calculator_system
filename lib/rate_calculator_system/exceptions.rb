9c4bf5cb290e6dc51ba0dcede00e1aafc12c9a1e
Abeer Salameh <abeer.salameh@gmail.com>
2016-03-20 17:54:43 +0000

    Tidy up exception strings

--- lib/rate_calculator_system/exceptions.rb
+++ lib/rate_calculator_system/exceptions.rb
@@ -1,11 +1,27 @@
-class RateCalculatorSystem
+module RateCalculatorSystem
   class InvalidHeaderError < StandardError
     def message
-      "Invalid Header in CSV file, your" \
-      "data should be structured as follows" \
-      "#{RateCalculatorSystem::InputParser::LENDER}," \
-      "#{RateCalculatorSystem::InputParser::RATE}," \
-      "#{RateCalculatorSystem::InputParser::AMOUNT}"
+      %(
+        Invalid Header in CSV file, your data should be structured as follows:
+        #{RateCalculatorSystem::InputParser::LENDER},
+        #{RateCalculatorSystem::InputParser::RATE},
+        #{RateCalculatorSystem::InputParser::AMOUNT}
+      )
+    end
+  end
+
+  class InvalidLoanError < StandardError
+    def message
+      %(
+        Invalid loan value, make sure the loan you enter is between 1000 and
+        15000, and that it is an increment of 100.
+      )
+    end
+  end
+
+  class NoFundsError < StandardError
+    def message
+      %(There aren't enough funds to satisfy the loan value, please try again.)
     end
   end
 end
