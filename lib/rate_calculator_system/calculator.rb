2008bae602451a3c5318afba1be609977a46d187
Abeer Salameh <abeer.salameh@gmail.com>
2016-03-20 02:55:59 +0000

    Add calculations for rate in calculator class
    We first calculate the total amount needed from all lenders with monthly compounding then we calculate the rate using that final value

--- lib/rate_calculator_system/calculator.rb
+++ lib/rate_calculator_system/calculator.rb
@@ -1,20 +1,74 @@
 class RateCalculatorSystem
   class Calculator
-    attr_reader :lenders, :loan
+    attr_reader :lenders, :requested_loan
 
-    MINIMUM = 1000
-    MAXIMUM = 15_000
+    MINIMUM_AMOUNT = 1000
+    MAXIMUM_AMOUNT = 15_000
+    LOAN_PERIOD = 3
 
-    def initialize(loan, lenders)
-      @loan = loan
-      @lenders = lenders
+    def initialize(requested_loan, lenders)
+      @requested_loan = requested_loan
+      @lenders = sort_for!(lenders)
     end
 
     def loan_valid?
-      Float(loan) &&
-        loan >= MINIMUM &&
-        loan <= MAXIMUM &&
-        loan % 100 == 0
+      Float(requested_loan) &&
+        requested_loan >= MINIMUM_AMOUNT &&
+        requested_loan <= MAXIMUM_AMOUNT &&
+        requested_loan % 100 == 0
+    end
+
+    def funds_available?
+      requested_loan <= total_funds
+    end
+
+    def evaluate_final_rate
+      "Requested amount: £#{requested_loan}\n" \
+      "Rate: #{rate}%\n" \
+      "Monthly repayment: £#{monthly_payment}\n" \
+      "Total repayment: £#{total_payment}\n"
+    end
+
+    private
+
+    def sort_for!(array)
+      array.sort! { |a, b| a.rate <=> b.rate }
+    end
+
+    def total_funds
+      lenders.inject(0) { |a, e| a + e.amount }
+    end
+
+    def rate
+      (((total_payment / requested_loan)**(1 / LOAN_PERIOD.to_f) - 1) * 100).round(1)
+    end
+
+    def total_payment
+      @_total_payment ||= (
+        loan_value = requested_loan
+        evaluate_total(loan_value).round(2)
+      )
+    end
+
+    def evaluate_total(loan_value)
+      lenders.inject(0) do |total, lender|
+        return total if loan_value <= 0
+        total += monthly_compounding_interest(amount(loan_value, lender.amount), lender.rate)
+        loan_value -= lender.amount
+        total
+      end
+    end
+
+    def monthly_compounding_interest(amount, interest_rate)
+      amount * (1 + interest_rate)**LOAN_PERIOD
+    end
+
+    def amount(loan_value, lender_amount)
+      loan_value <= lender_amount ? loan_value : lender_amount
+    end
+
+    def monthly_payment
+      (total_payment / 12).round(2)
     end
   end
 end
--- spec/rate_calculator_system/calculator_spec.rb
+++ spec/rate_calculator_system/calculator_spec.rb
@@ -3,42 +3,41 @@ require "spec_helper"
 RSpec.describe RateCalculatorSystem::Calculator do
   let(:lenders) do
     [
-      RateCalculatorSystem::Lender.new(name: "Bob", rate: 0.075, amount: 640),
-      RateCalculatorSystem::Lender.new(name: "Jane", rate: 0.069, amount: 480),
-      RateCalculatorSystem::Lender.new(name: "Fred", rate: 0.071, amount: 520),
-      RateCalculatorSystem::Lender.new(name: "Mary", rate: 0.104, amount: 170),
       RateCalculatorSystem::Lender.new(name: "John", rate: 0.081, amount: 320),
+      RateCalculatorSystem::Lender.new(name: "Mary", rate: 0.104, amount: 170),
+      RateCalculatorSystem::Lender.new(name: "Angela", rate: 0.071, amount: 60),
+      RateCalculatorSystem::Lender.new(name: "Fred", rate: 0.071, amount: 520),
       RateCalculatorSystem::Lender.new(name: "Dave", rate: 0.074, amount: 140),
-      RateCalculatorSystem::Lender.new(name: "Angela", rate: 0.071, amount: 60)
+      RateCalculatorSystem::Lender.new(name: "Jane", rate: 0.069, amount: 480),
+      RateCalculatorSystem::Lender.new(name: "Bob", rate: 0.075, amount: 640)
     ]
   end
-
-  let(:loan) { 1100 }
 
   subject { described_class.new(loan, lenders) }
 
   describe "loan_valid?" do
+    let(:loan) { 1100 }
     it "returns true if loan is valid" do
       expect(subject.loan_valid?).to be_truthy
     end
 
     context "less than minimum set value" do
       let(:loan) { 900 }
-      it "raises an error" do
+      it "returns false" do
         expect(subject.loan_valid?).to be_falsey
       end
     end
 
     context "more than maximum set value" do
       let(:loan) { 16_000 }
-      it "raises an error" do
+      it "returns false" do
         expect(subject.loan_valid?).to be_falsey
       end
     end
 
     context "not an increment of 100" do
       let(:loan) { 1101 }
-      it "raises an error" do
+      it "returns false" do
         expect(subject.loan_valid?).to be_falsey
       end
     end
@@ -47,6 +46,77 @@ RSpec.describe RateCalculatorSystem::Calculator do
       let(:loan) { "Boom" }
       it "raises an error" do
         expect { subject.loan_valid? }.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  describe "funds_available?" do
+    context "loan is more than available funds" do
+      let(:loan) { 5000 }
+      it "returns false" do
+        expect(subject.funds_available?).to be_falsey
+      end
+    end
+
+    context "loan is less than available funds" do
+      let(:loan) { 2000 }
+      it "returns true" do
+        expect(subject.funds_available?).to be_truthy
+      end
+    end
+  end
+
+  describe "evaluate_final_rate" do
+    let(:message) do
+      "Requested amount: £#{loan}\n" \
+      "Rate: #{rate}%\n" \
+      "Monthly repayment: £#{monthly_payment}\n" \
+      "Total repayment: £#{total_payment}\n"
+    end
+
+    context "loan amount less than one lender amount" do
+      let(:lenders) do
+        [
+          RateCalculatorSystem::Lender.new(name: "Bob", rate: 0.075, amount: 1640),
+          RateCalculatorSystem::Lender.new(name: "John", rate: 0.081, amount: 320)
+        ]
+      end
+
+      let(:loan) { 1000 }
+      let(:rate) { 7.5 }
+      let(:monthly_payment) { 103.53 }
+      let(:total_payment) { 1242.30 }
+
+      it "outputs correct rate and totals in message" do
+        expect(subject.evaluate_final_rate).to eq(message)
+      end
+    end
+
+    context "loan amount more than one lender amount" do
+      let(:loan) { 2300 }
+      let(:rate) { 7.5 }
+      let(:monthly_payment) { 238.33 }
+      let(:total_payment) { 2860.01 }
+
+      it "outputs correct rate and totals in message" do
+        expect(subject.evaluate_final_rate).to eq(message)
+      end
+    end
+
+    context "loan amount equal to one lender amount" do
+      let(:lenders) do
+        [
+          RateCalculatorSystem::Lender.new(name: "Bob", rate: 0.075, amount: 640),
+          RateCalculatorSystem::Lender.new(name: "John", rate: 0.081, amount: 320)
+        ]
+      end
+
+      let(:loan) { 640 }
+      let(:rate) { 7.5 }
+      let(:monthly_payment) { 66.26 }
+      let(:total_payment) { 795.07 }
+      it "outputs correct rate and totals in message" do
+        expect(subject.evaluate_final_rate).to eq(message)
       end
     end
   end
